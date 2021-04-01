ebee951f80e86e9ebe582032cb43b07af48c81fc
kcbavana <53190642+kcbavana@users.noreply.github.com>
2021-03-31 20:12:01 -0500

    completed Rules Scene
    actions needed

---
+++ fw-client/src/main/java/ClientRules.java
@@ -0,0 +1,24 @@
+
+public class ClientRules implements Initializable {
+	
+	@FXML
+	private BorderPane bPane;
+	
+	@FXML
+	private Text title;
+	
+	@FXML
+	private VBox rulesBox;
+	
+	@FXML
+	private Text rulesText;
+	
+	@FXML
+	private HBox menuBox;
+	
+	@FXML
+	private Button backButton;
+	
+	@FXML
+	private Button exitButton;
+}

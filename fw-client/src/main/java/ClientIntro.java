96dc1d3227a20d4b4aee325ac3fb46b8ac1ac4ba
Cody Hasty <chasty2@vorel.localdomain>
2021-03-31 21:33:44 -0500

    worked out scene change mechanics

--- fw-client/src/main/java/ClientIntro.java
+++ fw-client/src/main/java/ClientIntro.java
@@ -1,23 +1,27 @@
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
-
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class ClientIntro implements Initializable {
 	
 	public Client client;
+	public HashMap<String,Scene> scenes;
 	
 	@FXML
 	private BorderPane bPane;
@@ -50,14 +54,32 @@ public class ClientIntro implements Initializable {
 	}
 	
 	/*
+	 * Constructor, passes SceneMap to this class
+	 */
+	public ClientIntro(HashMap<String,Scene> sceneMap) {
+		scenes = sceneMap;
+	}
+	
+	/*
 	 * Connect to server 
 	 */
 	public void connectMethod(ActionEvent e) throws IOException {
-		client = new Client(data -> {
+		/*client = new Client(data -> {
 			Platform.runLater(() -> {
 				System.out.println(data.toString());
 			});
 		});
-		client.start();
+		client.start();*/
+		
+		//Change Scene
+		Node node=(Node) e.getSource();
+		Stage stage=(Stage) node.getScene().getWindow();
+		Parent root = FXMLLoader.load(getClass().getResource("/FXML/CategoryScene.fxml"));/* Exception */
+		Scene scene = new Scene(root);
+		//scene.getStylesheets().add("/styles/CategoryScene.css");
+		stage.setScene(scene);
+		stage.show();
+		
 	}
+
 }
--- fw-client/src/main/java/JavaFXClient.java
+++ fw-client/src/main/java/JavaFXClient.java
@@ -1,16 +1,19 @@
+import java.util.HashMap;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class JavaFXClient extends Application {
 
+	public static HashMap<String,Scene> sceneMap;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
@@ -19,7 +22,10 @@ public class JavaFXClient extends Application {
 	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+
+		// Build sceneMap
+		
+		
 		primaryStage.setTitle("Fortune Wheel Client");
 		Parent root = FXMLLoader.load(getClass().getResource("/FXML/ClientIntro.fxml"));
 		Scene scene = new Scene(root, 500,500);
@@ -36,5 +42,6 @@ public class JavaFXClient extends Application {
             }
         });
 	}
-
+	
+	// public HashMap<String,Scene> buildSceneMap() {}
 }
